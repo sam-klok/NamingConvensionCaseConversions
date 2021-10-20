babae55373a2beb969bf4c2244c14c90384b97f7
sam-klok <sklok11@gmail.com>
2021-10-20 16:41:13 -0700

    Initial commit

---
+++ NamingConvensionCaseConversions/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace NamingConvensionCaseConversions
+{
+    public static class StringExtension
+    {
+        public static string ToTiltleCase(this string s)
+        {
+            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+            var r = ti.ToTitleCase(s);
+            return r;
+        }
+
+        public static string ToTiltleCase2(this string s) =>
+            CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s);
+        public static string ToTrim(this string s) => s.Trim();
+
+
+        public static string ToCamelCase(this string s)
+        {
+            var sb = new StringBuilder();
+
+            s.Split(' ')
+                .ToList()
+                .ForEach(w =>
+                {
+                    sb.Append(char.ToUpper(w[0]));
+                    sb.Append(w.Substring(1));
+                    sb.Append(" ");
+                });
+
+            var t = sb.ToString().Trim();
+            var r = char.ToLower(t[0]) + t.Substring(1);
+
+            return r;
+        }
+
+        public static string ToCamelCaseV2(this string s)
+        {
+            var t = s.Split(' ')
+                .Select(w =>
+                    char.ToUpper(w[0])
+                    + w.Substring(1)
+                    + " ")
+                .Aggregate((i, j) => i + "" + j)   // we had .Join(" ") in older version of .Net
+                .Trim();
+
+            var r = char.ToLower(t[0]) + t.Substring(1);
+
+            return r;
+        }
+    }
+
+    class Program
+    {
+
+        const string TESTSTRING = "The Quick Brown Fox Jumps over the Lazy Dog";  // TitleCase example
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Test:         {TESTSTRING}");
+
+            Console.WriteLine($"Camel case:   {TESTSTRING.ToCamelCase()}");
+            Console.WriteLine($"Camel case 2: {TESTSTRING.ToCamelCaseV2()}");
+
+            Console.WriteLine($"Title case:   {TESTSTRING.ToTiltleCase()}");
+        }
+
+        //private static string ConvertToCamelCase(string s)
+        //{
+        //    var sb = new StringBuilder();
+
+        //    s.Split(' ')
+        //        .ToList()
+        //        .ForEach(w => sb.Append(char.ToUpper(w[0]) + w.Substring(1)));
+
+        //    var t = sb.ToString();
+        //    var r = char.ToLower(t[0]) + t.Substring(1);
+
+        //    return r;
+        //}
+
+
+        //private static string ConvertToTiltleCase(string s)
+        //{
+        //    TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+        //    var r = ti.ToTitleCase(s);
+        //    return r;
+        //}
+
+
+
+
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+1) Playing with lambdas
+https://stackoverflow.com/questions/5653703/can-a-c-sharp-lambda-expression-have-more-than-one-statement/5653749
+
+2) Playing with string case conversions
+https://docs.microsoft.com/en-us/dotnet/api/system.globalization.textinfo?view=net-5.0
+
+thank you
+sam (serge) klok
